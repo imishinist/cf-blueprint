0265398db743696bc8cd8557403629174a562880
Taisuke Miyazaki <imishinist@gmail.com>
2021-02-05 23:01:25 +0900

    parse jsonnet and add jsonnet params

--- deploy.py
+++ deploy.py
@@ -3,16 +3,31 @@
 
 import boto3
 import json
+import _jsonnet
 import fire
 
-class CFnDeploy(object):
-    def deploy(self, stack, template_path, params_path):        
-        client = boto3.client('cloudformation')
-        with open(template_path) as f:
-            s = f.read()
+import os.path
 
+class CFnDeploy(object):
+    def __load_jsonnet(self, params_path):
         with open(params_path) as f:
-            params = json.load(f)
+            jsonnet_str = f.read()
+            
+        json_str = _jsonnet.evaluate_snippet(
+            "snippet", jsonnet_str)
+
+        return json.loads(json_str)
+
+    def __load_json(self, params_path):
+        with open(params_path) as f:
+            return json.load(f)
+
+    def __load_cfn_params(self, params_path):
+        ext = os.path.splitext(params_path)[1]
+        if ext == ".jsonnet":
+            params = self.__load_jsonnet(params_path)
+        else:
+            params = self.__load_json(params_path)
 
         cfn_params = []
         for key in params:
@@ -23,11 +38,19 @@ class CFnDeploy(object):
                     'UsePreviousValue': False,
                 }
             )
+        return cfn_params
+
+    def deploy(self, stack, template_path, params_path):        
+        client = boto3.client('cloudformation')
+        with open(template_path) as f:
+            s = f.read()
+
+        params = self.__load_cfn_params(params_path)
 
         response = client.create_stack(
             StackName=stack,
             TemplateBody=s,
-            Parameters=cfn_params,
+            Parameters=params,
             Capabilities=['CAPABILITY_IAM', 'CAPABILITY_AUTO_EXPAND'])
         print(response)
 
